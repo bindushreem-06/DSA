c706c372ce62ae35495a6c80fb69e78504f5e34f
Bindushree <bindushreem06@gmail.com>
2025-11-29 09:34:25 +0530

    Update

---
+++ src/DSAonHeapAss3/MaxHeapPriorityQueue.java
@@ -0,0 +1,21 @@
+package DSAonHeapAss3;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class MaxHeapPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+
+        pq.add(10);
+        pq.add(5);
+        pq.add(20);
+        pq.add(1);
+
+        System.out.println("Max-Heap PQ: " + pq);
+        System.out.println("Peek (Max): " + pq.peek());
+        System.out.println("Removed: " + pq.poll());
+        System.out.println("After Poll: " + pq);
+    }
+}
+
